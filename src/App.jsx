9aedf92ba61a05908ee0c47f4dda8d3ac849a5ad
lrag14 <laurentrober@gmail.com>
2023-05-11 22:37:29 +0200

    map on /character op

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 //PAGES************
 import CharactersList from "./pages/CharactersList.jsx";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route
-					path='/'
-					element={<CharactersList />}
-				/>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CharactersList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/CharactersList.jsx
+++ src/pages/CharactersList.jsx
@@ -1,29 +1,43 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const CharactersList = () => {
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await axios.get(
-					"https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=jZHCwAvMJLbKEUaU"
-				);
-				console.log(response.data);
-				setData(response.data);
-				setIsLoading(false);
-			} catch (error) {
-				console.log(error.message);
-			}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/characters");
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
 
-			setIsLoading(false);
-		};
-		fetchData();
-	}, []);
-
-	return isLoading ? <p>Loading ..</p> : <h1>ListPersonnage</h1>;
+  return isLoading ? (
+    <p>Loading ..</p>
+  ) : (
+    <div>
+      {data.results.map((character) => {
+        return (
+          <article key={character.id}>
+            <h2>{character.name}</h2>
+            <img
+              src={
+                character.thumbnail.path + "." + character.thumbnail.extension
+              }
+              alt={character.name}
+            />
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CharactersList;
